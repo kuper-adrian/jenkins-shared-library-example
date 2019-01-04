178707d4db8187d368c9f39fed173ce18744d17f
Adrian Kuper <adrian.kuper@hotmail.de>
2019-01-04 11:14:23 +0100

    added some comments

--- src/org/somecompany/IStepExecutor.groovy
+++ src/org/somecompany/IStepExecutor.groovy
@@ -1,5 +1,8 @@
 package org.somecompany
 
+/**
+ * Interface for calling any necessary Jenkins steps. This will be mocked in unit tests.
+ */
 interface IStepExecutor {
     int sh(String command)
     void error(String message)
